d8c7974a1aafb309c78543fba36f8b98ee64c72e
= <jackcwong2007@gmail.com>
2026-06-08 01:07:46 -0400

    facial landmarks detected

--- video.py
+++ video.py
@@ -1,4 +1,50 @@
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import drawing_utils
+from mediapipe.tasks.python.vision import drawing_styles
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  face_landmarks_list = detection_result.face_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected faces to visualize.
+  for idx in range(len(face_landmarks_list)):
+    face_landmarks = face_landmarks_list[idx]
+
+    # Draw the face landmarks.
+
+
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_TESSELATION,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=drawing_styles.get_default_face_mesh_tesselation_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=drawing_styles.get_default_face_mesh_contours_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_LEFT_IRIS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_RIGHT_IRIS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
+
+  return annotated_image
 
 
 def main():
@@ -7,19 +53,35 @@ def main():
         print("Cannot open camera")
         return
 
+    # STEP 2: Create an FaceLandmarker object.
+    base_options = python.BaseOptions(model_asset_path='face_landmarker_v2_with_blendshapes.task')
+    options = vision.FaceLandmarkerOptions(base_options=base_options,
+                                        output_face_blendshapes=True,
+                                        output_facial_transformation_matrixes=True,
+                                        num_faces=1)
+    detector = vision.FaceLandmarker.create_from_options(options)
     while True:
         ret, frame = cap.read()
         if not ret:
             print("Failed to grab frame")
             break
+        
 
-        cv2.imshow("Camera", frame)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+
+        # Detect landmarks
+        detection_result = detector.detect(image)
+
+        # STEP 5: Process the detection result. In this case, visualize it.
+        annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+        cv2.imshow("Camera", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
